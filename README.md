dbfde770645aaf3f46627ba8ee63e7256f346015
unknown <saif_dar@outlook.com>
2018-08-02 12:05:50 -0700

    submission

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Eat-Da-Burger
+# burger
+
+This is a simple restaurant app built with node express, mysql, and handlebars.
+
+It follows the MVC pattern, with a model, views, controller.
+
+The model calls out to an ORM to get, add, and update data in mysql database.
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Chile-Stuffed Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burgers with Spicy Pickle Sauce');
+INSERT INTO burgers (burger_name) VALUES (' CHEESEBURGER WITH GRILLED ONIONS');
+INSERT INTO burgers (burger_name) VALUES ('THE ATOMIC BURGER');
